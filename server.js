26ffd393beba136502b71691030b8c75aebfd128
KvTsira <kvtsira@gmail.com>
2022-02-19 23:33:41 -0500

    create queries

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,21 @@ const app = express();
 //express midddleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+//connect to database
+const db= mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'WelcomeMondays1806!',
+        database: 'election'
+    },
+    console.log('Connected to the election database')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 //get route
 app.get('/', (req, res) => {
